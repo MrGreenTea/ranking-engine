57a53d96b444fbdc97b171fa4a64f88751cd5c52
Jonas Bulik <jonas@bulik.dev>
2025-01-19 22:03:41 +0100

    Better e2e tests

--- e2e/ranking.test.ts
+++ e2e/ranking.test.ts
@@ -1,49 +1,80 @@
 import { expect, test } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
-// copied from https://stackoverflow.com/a/2450976
-function shuffle<T>(array: T[]) {
-	// Fisher-Yates shuffle
-	let currentIndex = array.length;
+/**
+ * @description
+ *
+ * Verifies that the ranking engine correctly reorders items based on the
+ * user's input.
+ *
+ * This test generates a list of 10 random words,
+ * enters them into the ranking engine,
+ * and then verifies that the output is sorted correctly.
+ * We use alphabetical order as ranking.
+ */
 
-	// While there remain elements to shuffle...
-	while (currentIndex != 0) {
-		// Pick a remaining element...
-		const randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex--;
+// ranks 2, 3, 5, 10 and 25 items correctly
+[2, 3, 5, 10, 25].forEach((i) => {
+	test(`Ranks ${i} items correctly`, async ({ page }) => {
+		const items = Array.from({ length: i }, () => faker.word.noun());
+		// we sort the items alphabetically to better understand the output
+		// this is not necessary but helps us to verify the output
+		const sortedItems = [...items].sort();
 
-		// And swap it with the current element.
-		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-	}
-}
+		await page.goto('/');
 
-test('Ranks correctly', async ({ page }) => {
-	const sortedItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	const items = [...sortedItems];
-	shuffle(items);
-
-	await page.goto('/');
-
-	// enter randomized items
-	for (const item of items) {
-		await page.getByPlaceholder('Add an item...').fill(item.toString());
-		await page.getByRole('button', { name: 'Add' }).click();
-	}
-
-	await page.getByRole('button', { name: 'Start Sorting' }).click();
-	while (await page.getByRole('heading', { name: 'Compare items' }).isVisible()) {
-		const button1 = await page.getByTestId('comparison-buttons').getByRole('button').first();
-		const button2 = await page.getByTestId('comparison-buttons').getByRole('button').last();
-		const button1Number = Number(await button1.textContent());
-		const button2Number = Number(await button2.textContent());
-
-		if (button1Number < button2Number) {
-			await button1.click();
-		} else {
-			await button2.click();
+		// enter randomized items
+		for (const item of items) {
+			await page.getByPlaceholder('Add an item...').fill(item.toString());
+			await page.getByRole('button', { name: 'Add' }).click();
 		}
-	}
 
-	await expect(page.getByRole('listitem')).toContainText(
-		sortedItems.map((item) => item.toString())
-	);
+		await page.getByRole('button', { name: 'Start Sorting' }).click();
+		while (await page.getByRole('heading', { name: 'Compare items' }).isVisible()) {
+			const button1 = await page.getByTestId('comparison-buttons').getByRole('button').first();
+			const button2 = await page.getByTestId('comparison-buttons').getByRole('button').last();
+			const button1Content = await button1.textContent();
+			const button2Content = await button2.textContent();
+
+			await (button1Content! < button2Content! ? button1 : button2).click();
+		}
+
+		await expect(page.getByRole('listitem')).toContainText(
+			sortedItems.map((item) => item.toString())
+		);
+	});
+});
+
+// test top 3, 5 and 10 items
+[3, 5, 10].forEach((i) => {
+	test(`Top ${i} items`, async ({ page }) => {
+		const items = Array.from({ length: 10 + i }, () => faker.word.noun());
+		// we sort the items alphabetically to better understand the output
+		// this is not necessary but helps us to verify the output
+		const sortedItems = [...items].sort();
+
+		await page.goto('/');
+
+		// enter randomized items
+		for (const item of items) {
+			await page.getByPlaceholder('Add an item...').fill(item.toString());
+			await page.getByRole('button', { name: 'Add' }).click();
+		}
+
+		await page.getByPlaceholder('Top K items (optional)').fill(i.toString());
+
+		await page.getByRole('button', { name: 'Start Sorting' }).click();
+		while (await page.getByRole('heading', { name: 'Compare items' }).isVisible()) {
+			const button1 = await page.getByTestId('comparison-buttons').getByRole('button').first();
+			const button2 = await page.getByTestId('comparison-buttons').getByRole('button').last();
+			const button1Content = await button1.textContent();
+			const button2Content = await button2.textContent();
+
+			await (button1Content! < button2Content! ? button1 : button2).click();
+		}
+
+		await expect(page.getByTestId('sorted-item')).toContainText(
+			sortedItems.slice(0, i).map((item) => item.toString())
+		);
+	});
 });
