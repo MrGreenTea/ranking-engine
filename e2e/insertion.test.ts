7d0179c7a8a49f0390368c0cf602896bbccf4abd
Jonas Bulik <jonas@bulik.dev>
2025-01-20 23:21:39 +0100

    test: more tests for insertion

--- e2e/insertion.test.ts
+++ e2e/insertion.test.ts
@@ -2,23 +2,28 @@ import { expect, test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { randomItemList, enterItems, sortItems } from './helpers';
 
-test('Add item after sorting', async ({ page }) => {
-	const items = randomItemList(3);
+[
+	{ items: ['A', 'B', 'C'], newItem: 'D', title: 'Insert last' },
+	{ items: ['B', 'C', 'D'], newItem: 'A', title: 'Insert first' },
+	{ items: ['A', 'C', 'D'], newItem: 'B', title: 'Insert middle' },
+	{ items: randomItemList(3), newItem: faker.word.noun(), title: 'Insert random (3 items)' },
+	{ items: randomItemList(5), newItem: faker.word.noun(), title: 'Insert random (5 items)' }
+].forEach(({ items, newItem, title }) => {
+	test(title, async ({ page }) => {
+		await page.goto('/');
 
-	await page.goto('/');
+		// enter randomized items
+		await enterItems(page, items);
 
-	// enter randomized items
-	await enterItems(page, items);
+		await page.getByRole('button', { name: 'Start Sorting' }).click();
+		await sortItems(page);
 
-	await page.getByRole('button', { name: 'Start Sorting' }).click();
-	await sortItems(page);
+		await page.getByPlaceholder('Insert new item').fill(newItem);
+		await page.getByRole('button', { name: 'Insert', exact: true }).click();
 
-	const newItem = faker.word.noun();
-	await page.getByPlaceholder('Insert new item').fill(newItem);
-	await page.getByRole('button', { name: 'Insert', exact: true }).click();
+		await sortItems(page);
 
-	await sortItems(page);
-
-	const sortedItems = [...items, newItem].sort();
-	await expect(page.getByRole('listitem')).toContainText(sortedItems);
+		const sortedItems = [...items, newItem].sort();
+		await expect(page.getByRole('listitem')).toContainText(sortedItems);
+	});
 });
