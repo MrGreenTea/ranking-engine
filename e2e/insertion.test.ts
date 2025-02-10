86199818d932247dd142109b6adb6dfad6fe87dd
Jonas Bulik <jonas@bulik.dev>
2025-02-10 10:10:35 +0100

    test: fix flaky insertion tests

--- e2e/insertion.test.ts
+++ e2e/insertion.test.ts
@@ -3,12 +3,28 @@ import { expect, test } from '@playwright/test';
 
 import { enterItems, randomItemList, sortItems } from './helpers';
 
+// In rare cases we would generate a new item that is already in the list,
+// which would break the test. This function makes sure we always get a new item.
+function uniqueNewItem(items: string[]) {
+	let newItem = faker.word.noun();
+	while (items.includes(newItem)) {
+		newItem = faker.word.noun();
+	}
+	return { items, newItem };
+}
+
 [
 	{ items: ['A', 'B', 'C'], newItem: 'D', title: 'Insert last' },
 	{ items: ['B', 'C', 'D'], newItem: 'A', title: 'Insert first' },
 	{ items: ['A', 'C', 'D'], newItem: 'B', title: 'Insert middle' },
-	{ items: randomItemList(3), newItem: faker.word.noun(), title: 'Insert random (3 items)' },
-	{ items: randomItemList(5), newItem: faker.word.noun(), title: 'Insert random (5 items)' }
+	{
+		...uniqueNewItem(randomItemList(3)),
+		title: 'Insert random (3 items)'
+	},
+	{
+		...uniqueNewItem(randomItemList(5)),
+		title: 'Insert random (5 items)'
+	}
 ].forEach(({ items, newItem, title }) => {
 	test(title, async ({ page }) => {
 		await page.goto('/');
